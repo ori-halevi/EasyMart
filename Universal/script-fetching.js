d3285c4d5864b64eb385dc3c54bb46d92e1d0f7e
binyamin-zaidman <bl8843@gmail.com>
2024-11-06 09:35:50 +0200

    אחרון

--- Universal/script-fetching.js
+++ Universal/script-fetching.js
@@ -1,7 +1,8 @@
 // getAllUsers() returns a list of users objects.
 function getAllUsers() {
   return fetch("http://localhost:3000/users")
-  .then((res) => res.json()); // Fix typo here
+  .then((res) => {
+    res.json()}); // Fix typo here
 }
 
 // isUserExistInDB() checks if a user exists in the database.
@@ -51,7 +52,6 @@ async function addUserToDB(user) {
     console.error("Error adding user:", error);
   }
 }
-
 
 
 
